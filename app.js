9bbd840a0a058b2b6738a5f322c83ec5c1699412
Maria Clara <claarasantana@gmail.com>
2017-03-22 14:00:33 -0300

    getting credentials from encrypted context

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var Twitter = require('twitter');
 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
-
 module.exports = function(ctx, cb) {
-	var tweet = 'Just released v' + ctx.body.release.tag_name + ' of ' + ctx.body.repository.name + '. Check it out: ' + ctx.body.release.html_url;
+
+  var client = new Twitter({
+    consumer_key: ctx.secrets.consumer_key,
+    consumer_secret: ctx.secrets.consumer_secret,
+    access_token_key: ctx.secrets.access_token_key,
+    access_token_secret: ctx.secrets.access_token_secret
+  });
+
+  var tweet = 'Just released v' + ctx.body.release.tag_name + ' of ' + ctx.body.repository.name + '. Check it out: ' + ctx.body.release.html_url;
 
 	client.post('statuses/update', { status: tweet },  function(error, tweet, response) {
   	if(error) throw error;
+    else return;
 	});
 };
